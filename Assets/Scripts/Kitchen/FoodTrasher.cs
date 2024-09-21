2542642328952c19a0042a547d567111d3de99a6
Augrider <alotodem@gmail.com>
2024-09-21 15:36:55 +0300

    Шаг 01: Написаны недостающие методы

--- Assets/Scripts/Kitchen/FoodTrasher.cs
+++ Assets/Scripts/Kitchen/FoodTrasher.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 
 using JetBrains.Annotations;
 
-namespace CookingPrototype.Kitchen {
-	[RequireComponent(typeof(FoodPlace))]
-	public sealed class FoodTrasher : MonoBehaviour {
+namespace CookingPrototype.Kitchen
+{
+    [RequireComponent(typeof(FoodPlace))]
+    public sealed class FoodTrasher : MonoBehaviour
+    {
 
-		FoodPlace _place = null;
-		float     _timer = 0f;
+        FoodPlace _place = null;
+        float _timer = 0f;
 
-		void Start() {
-			_place = GetComponent<FoodPlace>();
-			_timer = Time.realtimeSinceStartup;
-		}
+        void Start()
+        {
+            _place = GetComponent<FoodPlace>();
+            _timer = Time.realtimeSinceStartup;
+        }
 
-		/// <summary>
-		/// Освобождает место по двойному тапу если еда на этом месте сгоревшая.
-		/// </summary>
-		[UsedImplicitly]
-		public void TryTrashFood() {
-			throw new NotImplementedException("TryTrashFood: this feature is not implemented");
-		}
-	}
+        /// <summary>
+        /// Освобождает место по двойному тапу если еда на этом месте сгоревшая.
+        /// </summary>
+        [UsedImplicitly]
+        public void TryTrashFood()
+        {
+            //If overcooked food is present - remove it entirely
+            //Else - do nothing
+            if (!_place.IsFree && _place.CurFood.CurStatus == Food.FoodStatus.Overcooked)
+                _place.FreePlace();
+        }
+    }
 }
